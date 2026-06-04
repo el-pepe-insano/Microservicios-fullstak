e643a25d8332492fae9e3cc5e63ab5bbc5943992
Acer <di.laraa@duocuc.cl>
2026-06-04 15:02:21 -0400

    feat(pagos): crear microservicio GestionPagos con validacion cruzada con HistorialDePedidos

---
+++ Microservicios-fullstak/GestionPagos/src/main/java/com/mediexpress/GestionPagos/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+﻿package com.mediexpress.GestionPagos.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClientPedidos(@Value("${servicio.pedidos.url}") String url) {
+        return WebClient.builder().baseUrl(url).build();
+    }
+    @Bean
+    public WebClient webClientUsuarios(@Value("${servicio.usuarios.url}") String url) {
+        return WebClient.builder().baseUrl(url).build();
+    }
+}
